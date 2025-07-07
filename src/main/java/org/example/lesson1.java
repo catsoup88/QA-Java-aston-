2c39416d0a2ecc1207ef9264d94a66e52b0e821e
catsoup88 <yakutingleb@mail.ru>
2025-07-07 18:12:56 +0300

    add new branch with Lesson_1

---
+++ src/main/java/org/example/lesson1.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class lesson1 {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -10, b = 50;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    /*public static void printColor() {
+        int value = 222;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (0 < value <= 100) {
+            System.out.println("Желтый");
+        } else (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 5, b = 55;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void twoNumbers() {
+
+    }
+
+     */
+}
