077f8dab338e0357b8ffd4a7b9685b96813180a2
catsoup88 <yakutingleb@mail.ru>
2025-07-10 19:19:53 +0300

    add new branch with Lesson_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,44 @@ public class Main {
     public static void main(String[] args) {
         lesson1.printThreeWords();
         lesson1.checkSumSign();
+        lesson1.printColor();
+        lesson1.compareNumbers();
+        System.out.println(lesson1.twoNumbers(6, 22));
+        lesson1.plusminus(-5);
+        System.out.println(lesson1.number(44));
+        lesson1.razraz("dfgdfgdf", 4);
+        System.out.println(lesson1.year(2000));
+        int[] arr = new int[]{1, 0, 1, 0, 1, 0, 1, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            System.out.println(arr[i]);
         }
+        int[] arr2 = new int[100];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = i + 1;
+            System.out.println(arr2[i]);
+        }
+        int[] arr3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) {
+                arr3[i] = arr3[i] * 2;
+            }
+            System.out.println(arr3[i]);
+        }
+        int[][] table = new int[5][5];
+        for (int i = 0; i < table.length; i++) {
+            table[i][arr.length - i - 1] = 1;
+        }
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][i] = 1;
+                System.out.println(table[i][j] + "");
+            }
+        }
+       lesson1.array(3,3);
     }
+}
--- src/main/java/org/example/lesson1.java
+++ src/main/java/org/example/lesson1.java
@@ -16,13 +16,13 @@ public class lesson1 {
         }
     }
 
-    /*public static void printColor() {
+    public static void printColor() {
         int value = 222;
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (0 < value <= 100) {
+        } else if (0 < value && value <= 100) {
             System.out.println("Желтый");
-        } else (value > 100) {
+        } else {
             System.out.println("Зеленый");
         }
     }
@@ -35,9 +35,44 @@ public class lesson1 {
             System.out.println("a < b");
         }
     }
-    public static void twoNumbers() {
 
+    public static boolean twoNumbers(int a, int b) {
+        int sum = a + b;
+        if (sum > 10 && sum < 20) return true;
+        else return false;
     }
 
-     */
+    public static void plusminus(int a) {
+        if (a < 0) {
+            System.out.println("Число отрицательное");
+        } else {
+            System.out.println("Число положительное");
+        }
+    }
+
+    public static boolean number(int a) {
+        if (a < 0) return false;
+        else return true;
+    }
+
+    public static void razraz(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
+        }
+    }
+
+    public static boolean year(int a) {
+        return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
+    }
+
+    public static int[] array(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        return arr;
+    }
 }
