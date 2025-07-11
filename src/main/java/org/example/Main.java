a6ab12bc2822469c193061f6ff08209d20641ee9
catsoup88 <yakutingleb@mail.ru>
2025-07-11 16:19:41 +0300

    add new branch with Lesson_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,37 +13,10 @@ public class Main {
         System.out.println(lesson1.number(44));
         lesson1.razraz("dfgdfgdf", 4);
         System.out.println(lesson1.year(2000));
-        int[] arr = new int[]{1, 0, 1, 0, 1, 0, 1, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) {
-                arr[i] = 0;
-            } else {
-                arr[i] = 1;
-            }
-            System.out.println(arr[i]);
-        }
-        int[] arr2 = new int[100];
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = i + 1;
-            System.out.println(arr2[i]);
-        }
-        int[] arr3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6) {
-                arr3[i] = arr3[i] * 2;
-            }
-            System.out.println(arr3[i]);
-        }
-        int[][] table = new int[5][5];
-        for (int i = 0; i < table.length; i++) {
-            table[i][arr.length - i - 1] = 1;
-        }
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                table[i][i] = 1;
-                System.out.println(table[i][j] + "");
-            }
-        }
-       lesson1.array(3,3);
+        lesson1.invertArray();
+        lesson1.fillArray();
+        lesson1.lesssix();
+        lesson1.diagonal();
+        lesson1.array(3,3);
     }
 }
--- src/main/java/org/example/lesson1.java
+++ src/main/java/org/example/lesson1.java
@@ -1,12 +1,13 @@
 package org.example;
 
 public class lesson1 {
+//1
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-
+//2
     public static void checkSumSign() {
         int a = -10, b = 50;
         if (a + b >= 0) {
@@ -15,7 +16,7 @@ public class lesson1 {
             System.out.println("Сумма отрицательная");
         }
     }
-
+//3
     public static void printColor() {
         int value = 222;
         if (value <= 0) {
@@ -26,7 +27,7 @@ public class lesson1 {
             System.out.println("Зеленый");
         }
     }
-
+//4
     public static void compareNumbers() {
         int a = 5, b = 55;
         if (a >= b) {
@@ -35,13 +36,12 @@ public class lesson1 {
             System.out.println("a < b");
         }
     }
-
+//5
     public static boolean twoNumbers(int a, int b) {
-        int sum = a + b;
-        if (sum > 10 && sum < 20) return true;
-        else return false;
-    }
+        return a + b > 10 && a + b < 20;
 
+    }
+//6
     public static void plusminus(int a) {
         if (a < 0) {
             System.out.println("Число отрицательное");
@@ -49,28 +49,65 @@ public class lesson1 {
             System.out.println("Число положительное");
         }
     }
-
+//7
     public static boolean number(int a) {
         if (a < 0) return false;
         else return true;
     }
-
+//8
     public static void razraz(String a, int b) {
         for (int i = 0; i < b; i++) {
             System.out.println(a);
         }
     }
-
+//9
     public static boolean year(int a) {
         return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
     }
-
+//10
+    public static void invertArray () {
+        int[] arr = new int[]{1, 0, 1, 0, 1, 0, 1, 1};
+        for (int i = 0; i < arr.length; i++) {
+                arr[i] = arr[i] == 0 ? 1 : 0;
+            System.out.print(arr[i]);
+        }
+    }
+//11
+    public static void fillArray () {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
+        }
+    }
+//12
+    public static void lesssix () {
+        int[] arr = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+            System.out.print(arr[i]);
+        }
+    }
+//13
+    public static void diagonal () {
+        int[][] table = new int[5][5];
+        for (int i = 0; i < table.length; i++) {
+            table[i][table.length - i - 1] = 1;
+        }
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][i] = 1;
+                System.out.println(table[i][j]);
+            }
+        }
+    }
+//14
     public static int[] array(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < len; i++) {
             arr[i] = initialValue;
-        }
-        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
         return arr;
