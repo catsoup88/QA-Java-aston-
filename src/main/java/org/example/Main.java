ea9598c86a6b2cc0774a0fd57233f8b615884079
catsoup88 <yakutingleb@mail.ru>
2025-07-14 17:32:05 +0300

    add new branch with lesson2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,24 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //2
+        Product[] productsArray = new Product[5];
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("iPhone 16 Pro", "15.09.2024", "Apple Inc.", "USA", 1299.00, false);
+        productsArray[2] = new Product("Samsung Galaxy Tab S10 FE", "03.10.2024 ", "Samsung", "Вьетнам", 1555.99, true);
+        productsArray[3] = new Product("Google Pixel 6 Pro", "28.10.2021", "Google Inc.", "USA", 339.99, false);
+        productsArray[4] = new Product("iPhone 15 Pro", "12.09.2023", "Apple Inc.", "USA", 899.00, true);
+        for (Product product : productsArray) {
+            product.info();
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //3
+        Park park = new Park("Гомельский парк");
+        Park.Attraction attraction1 = park.new Attraction("Колесо обозрения", "10.00 - 22.00", 15.00);
+        Park.Attraction attraction2 = park.new Attraction("Лебеди", "10.00 - 20.00", 10.00);
+        Park.Attraction attraction3 = park.new Attraction("Американские горки", "10.00 - 23.00", 25.00);
+        attraction1.infoPark();
+        attraction2.infoPark();
+        attraction3.infoPark();
         }
     }
-}
---
+++ src/main/java/org/example/Park.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Park {
+    protected String name;
+    public Park(String name) {
+        this.name = name;
+    }
+    public class Attraction {
+        private String attractionName;
+        private String workingHours;
+        private double Price;
+        public Attraction(String attractionName, String workingHours, double Price) {
+            this.attractionName = attractionName;
+            this.workingHours = workingHours;
+            this.Price = Price;
+        }
+        public void infoPark() {
+            System.out.println("Название:" + attractionName);
+            System.out.println("Время работы:" + workingHours);
+            System.out.println("Цена:" + Price + " руб.");
+            System.out.println(" ");
+        }
+    }
+}
